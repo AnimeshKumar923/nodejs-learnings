c815dbe96e5d35e08a99b320ca39655e1eeac8d0
Animesh Kumar <kumaranimesh923@gmail.com>
2025-07-09 14:30:16 +0530

    fix: add initial files, add index views

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("node:path");
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
+const { messages } = require("./routes/index");
 
 const hostname = "localhost";
 const PORT = 3000;
@@ -10,19 +11,17 @@ const PORT = 3000;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const links = [
-  { href: "/", text: "Home" },
-  { href: "about", text: "About" },
-];
-
-const users = ["Rose", "Cake", "Biff"];
+// const links = [
+//   { href: "/", text: "Home" },
+//   { href: "about", text: "About" },
+// ];
 
 app.get("/", (req, res) => {
-  res.render("index", { links: links, users: users });
+  res.render("index", { messages: messages });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about");
+app.get("/new", (req, res) => {
+  res.render("new");
 });
 
 app.listen(PORT, hostname, () => {
