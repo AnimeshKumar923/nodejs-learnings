0e4f9b9728ccb556e8d1b0c62e5ca9e4a3d2e028
Animesh Kumar <kumaranimesh923@gmail.com>
2025-07-17 16:06:19 +0530

    fix: remove commented code block

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,5 @@
 const db = require("../db/queries");
 
-// async function getUsernames(req, res) {
-//   res.send("Usernames: " + usernames.map((user) => user.username).join(", "));
-// }
-
 async function createUsernameGet(req, res) {
   // render the form
   console.log("usernames will be logged here - wip");
