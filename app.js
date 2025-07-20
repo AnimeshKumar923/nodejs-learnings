1a5b8e665de57b20998d81111f180ce9a932fd7a
Animesh Kumar <kumaranimesh923@gmail.com>
2025-07-20 10:03:58 +0530

    fix: move routes into separate file

--- app.js
+++ app.js
@@ -2,13 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 const assetsPath = path.join(__dirname, "public");
-const {
-  getUserById,
-  newMessageFormGet,
-  newMessageFormPost,
-  indexPageGet,
-} = require("./controllers");
-const CustomNotFoundError = require("./errors/CustomNotFoundError");
+const appRoute = require("./routes");
 
 app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
@@ -18,13 +12,7 @@ const PORT = process.env.PORT || 4000;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", indexPageGet);
-app.get("/new", newMessageFormGet);
-app.post("/new", newMessageFormPost);
-app.get("/message", () => {
-  throw new CustomNotFoundError("Page doesn't exist");
-});
-app.get("/message/:msgId", getUserById);
+app.use("/", appRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running at port: ${PORT}/`);
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,13 @@
-module.exports = {};
+const { Router } = require("express");
+const controller = require("../controllers/index");
+const appRoute = Router();
+
+appRoute.get("/", controller.indexPageGet);
+appRoute.get("/new", controller.newMessageFormGet);
+appRoute.post("/new", controller.newMessageFormPost);
+appRoute.get("/message", () => {
+  throw new CustomNotFoundError("Page doesn't exist");
+});
+appRoute.get("/message/:msgId", controller.getUserById);
+
+module.exports = appRoute;
