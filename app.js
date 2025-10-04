31973a62f737192466588ae906e724dbb2c19ace
Animesh Kumar <kumaranimesh923@gmail.com>
2025-10-04 18:15:53 +0530

    routes modularization

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 const path = require("node:path");
 const express = require("express");
-const session = require("express-session");
-const passport = require("passport");
-const { localStrategyConfig } = require("./auth/strategy");
-const appRoute = require("./routes");
-const {
-  deserializerFunction,
-  serializerFunction,
-} = require("./auth/serialization");
-const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
-const { PrismaClient } = require("./prisma/generated/prisma");
-const { allFolderOfUser } = require("./controllers");
+const routes = require("./routes");
+const models = require("./models");
 
 require("dotenv").config();
 
@@ -18,60 +9,19 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-let users = {
-  1: {
-    id: "1",
-    username: "Robin Wieruch",
-  },
-  2: {
-    id: "2",
-    username: "Dave Davids",
-  },
-};
-
-let messages = {
-  1: {
-    id: "1",
-    text: "Hello World",
-    userId: "1",
-  },
-  2: {
-    id: "2",
-    text: "By World",
-    userId: "2",
-  },
-};
-
-// app.use("/", appRoute);
-
-app.post("/users", (req, res) => {
-  return res.send("POST HTTP method on user resource");
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
 });
 
-app.put("/users/:userId", (req, res) => {
-  return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
-});
-
-app.delete("/users/:userId", (req, res) => {
-  return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
-});
-
-app.get("/users", (req, res) => {
-  return res.send(Object.values(users));
-});
-
-app.get("/users/:userId", (req, res) => {
-  return res.send(users[req.params.userId]);
-});
-
-app.get('/messages', (req, res) => {
-  return res.send(Object.values(messages));
-});
-
-app.get('/messages/:messageId', (req, res) => {
-  return res.send(messages[req.params.messageId]);
-});
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/messages", routes.message);
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
---
+++ models/index.js
@@ -0,0 +1,32 @@
+let users = {
+  1: {
+    id: "1",
+    username: "Animesh",
+  },
+  2: {
+    id: "2",
+    username: "Robin Wieruch",
+  },
+  3: {
+    id: "3",
+    username: "Dave Davids",
+  },
+};
+
+let messages = {
+  1: {
+    id: "1",
+    text: "Hello World",
+    userId: "1",
+  },
+  2: {
+    id: "2",
+    text: "By World",
+    userId: "2",
+  },
+};
+
+module.exports = {
+  users,
+  messages,
+};
